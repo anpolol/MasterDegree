915348971af123f51ffa335f4821f693518d4a14
anpolol <polinaspb@ya.ru>
2020-11-17 22:33:47 +0300

    Исправлен GCN. Добавлено сравнение с случаем классификации только на фичах, без обучения эмбедингов

--- Code/modules/model.py
+++ Code/modules/model.py
@@ -30,9 +30,13 @@ class Net(torch.nn.Module):
             self.convs.append(GATConv(256, out_channels))
                 
     def forward(self,x,adjs):
+        
         for i, (edge_index, _, size) in enumerate(adjs):
             x_target = x[:size[1]]  # Target nodes are always placed first.
-            x = self.convs[i]((x, x_target), edge_index)
+            if self.conv == 'GCN':
+                x = self.convs[i]((x), edge_index)
+            else:
+                x = self.convs[i]((x,x_target), edge_index)
             if i != self.num_layers - 1:
                 x = F.relu(x)
                 x = F.dropout(x, p=0.5, training=self.training)
